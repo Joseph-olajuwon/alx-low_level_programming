96bddfadc0cc851bb0db3d7cf1ea1e2e86a4f33c
Joseph-olajuwon <hollarjayjossy@gmail.com>
2023-09-08 12:26:16 -0700

    4-puts.c clear
    fe;igrw

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that's print a line with puts funtion
+ n
+* Return : 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
