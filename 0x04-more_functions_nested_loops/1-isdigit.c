011ce75fff44a254398d7ffac1a510e6210b2236
Joseph-olajuwon <hollarjayjossy@gmail.com>
2023-09-15 01:04:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h>"
+/**
+ * _indigit - function checks for digit (0 through 9)
+ * @c: inter to be checked
+ * Return: 1 if c is a digit, 0 if not
+ */
+int _indigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - funtions checks for a digits (0 through).
+ * @a: a - variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+	mul = a * b;
+	return (mul);
+}
